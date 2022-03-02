75ed84851b21b6a1cd1ce77d2eb8a685d6c97f4b
corentin jezierski <corentin.jezierski@gmail.com>
2022-03-02 19:05:18 +0100

    changement de boutons pour activer kle raccourci, maintenant c'est ctrl + b

--- my_shortcuts.js
+++ my_shortcuts.js
@@ -6,17 +6,17 @@ var isG = false;
 
 $(document).keydown(function(e)
 {
-	if (e.which == 87 || e.keyCode == 87)
+	if (e.which == 17 || e.keyCode == 17)
 	{ 
-		isG = true; // si la touche G a été pressée
+		isG = true; // si la touche CTRL a été pressée
 	}
 }).keyup(function(e)
 {
-	if ($('input:focus').length > 0 || $('textarea:focus').length > 0 || isG != true)
-	{ 
-		isG = false; // Si on se trouve dans un input, une textarea ou si on n'a pas pressé la touche G, on ne peut pas faire des raccourcis clavier
-		return false;
-	}
+	// if ($('input:focus').length > 0 || $('textarea:focus').length > 0 || isG != true)
+	// { 
+	// 	isG = false; // Si on se trouve dans un input, une textarea ou si on n'a pas pressé la touche CTRL, on ne peut pas faire des raccourcis clavier
+	// 	return false;
+	// }
 
 	if (e.keyCode == true)
 	{
@@ -29,23 +29,34 @@ $(document).keydown(function(e)
 
 	switch (key) // On regarde la deuxième touche pressée par l'utilisateur
 	{
-		// W + N
-		case 78:
-            research_google_text_highlighted()
+		// CTRL + B
+		case 66:
+            research_google_text_highlighted();
 			return false;
 			break;
+
 	}
 	
 	isG = false; // On réinitialise le booléen
 });
 
 function research_google_text_highlighted(){
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    navigator.clipboard.writeText(text);
-    window.open('https://google.com','_blank');
+	var selectedText = '';
+	// window.getSelection
+	if (window.getSelection) {
+		selectedText = window.getSelection();
+	}
+	// document.getSelection
+	else if (document.getSelection) {
+		selectedText = document.getSelection();
+	}
+	// document.selection
+	else if (document.selection) {
+		selectedText = document.selection.createRange().text;
+	}
+	if  (selectedText != ''){
+		navigator.clipboard.writeText(selectedText).then(res=>{
+			window.open('https://google.com','_blank');
+		});
+	}
 }
